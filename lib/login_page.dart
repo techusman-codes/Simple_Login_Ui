e8d5a5afe565c5b831009e798874ebb8ca82880f
Usman Umar Garba <ugarba202@gmail.com>
2025-03-04 23:57:04 +0100

    login buttons

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,9 +7,52 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Center(
-          child: Text('Login Ui'),
+        title: Text(''),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 30, 20, 30),
+        child: SizedBox(
+          height: 515,
+          width: 353,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome back!',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Please login to your account to countinue.',
+                style: TextStyle(
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.facebook),
+                    label: Text('Facebook'),
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.whatshot),
+                    label: Text('Twitter'),
+                    onPressed: () {},
+                    style: ButtonStyle(),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
