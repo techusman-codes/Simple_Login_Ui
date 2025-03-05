917d703d7bd3d4cd3d6a1e662fb610fd2c1b3eb1
Usman Umar Garba <ugarba202@gmail.com>
2025-03-05 16:52:08 +0100

    login UI

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,57 +1,253 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isChecked = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 30, 20, 30),
-        child: SizedBox(
-          height: 515,
-          width: 353,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Welcome back!',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   title: Text(''),
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 100, 20, 30),
+          child: SizedBox(
+            height: 515,
+            width: 353,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome back!',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                'Please login to your account to countinue.',
-                style: TextStyle(
-                  fontStyle: FontStyle.normal,
+                Text(
+                  'Please login to your account to countinue.',
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                children: [
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.facebook),
-                    label: Text('Facebook'),
-                    onPressed: () {},
+                SizedBox(
+                  height: 50,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ElevatedButton.icon(
+                        icon: Icon(
+                          Icons.facebook,
+                          color: Colors.blue,
+                          size: 28,
+                        ),
+                        label: Text(
+                          'Facebook',
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 30),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Colors.black, width: 0.5),
+                          ),
+                          overlayColor: Colors.white,
+                          elevation: 0,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton.icon(
+                        icon: Icon(
+                          Icons.alternate_email,
+                          color: Colors.blue,
+                          size: 28,
+                        ),
+                        label: Text(
+                          'Twitter',
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 40),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Colors.black, width: 0.5),
+                          ),
+                          overlayColor: Colors.white,
+                          elevation: 0,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    width: 40,
-                  ),
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.whatshot),
-                    label: Text('Twitter'),
-                    onPressed: () {},
-                    style: ButtonStyle(),
-                  ),
-                ],
-              )
-            ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Email Field
+                    Text(
+                      'Email Address',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    TextField(
+                      keyboardType:
+                          TextInputType.emailAddress, // Ensure proper keyboard
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 0.5,
+                          ),
+                        ),
+                        hintText: 'Enter your email',
+                        hintStyle: TextStyle(fontSize: 14),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+
+                    // Password Field
+                    Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    TextField(
+                      obscureText: true, // Hide password
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 0.5,
+                          ),
+                        ),
+                        hintText: 'Enter your password',
+                        hintStyle: TextStyle(fontSize: 14),
+
+                        suffixIcon: Icon(
+                          Icons.visibility_off,
+                          color: Colors.black,
+                          size: 14,
+                        ), // Eye Icon
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Checkbox(
+                              value: isChecked,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  isChecked = value!;
+                                });
+                              },
+                              focusColor: Colors.black,
+                              checkColor: Colors.black,
+                            ),
+                            Text('Remember Me here'),
+                          ],
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Handle forgot password logic
+                          },
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Handle login logic
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Colors.blue.shade400, // Button color
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    10), // Rounded corners
+                              ),
+                            ),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        RichText(
+                          text: TextSpan(
+                            text: "Don't have an account? ",
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                            children: [
+                              TextSpan(
+                                text: "Sign Up",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
